19a24c39b8c23be1e0f4f91b2a859dd4477976a9
Ying Wang <wangying@google.com>
2010-03-09 09:16:26 -0800

    Enable Proguard on Provision.
    Change-Id: Ie414908ba633b8a3664d1e6bc9f8792a8b04d15e

--- Android.mk
+++ Android.mk
@@ -8,6 +8,6 @@ LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_PACKAGE_NAME := Provision
 LOCAL_CERTIFICATE := platform
 
-LOCAL_PROGUARD_ENABLED := disabled
+LOCAL_PROGUARD_FLAGS := -include $(LOCAL_PATH)/proguard.flags
 
 include $(BUILD_PACKAGE)
